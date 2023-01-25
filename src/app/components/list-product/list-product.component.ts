35c050dcd086862c8eda131d6019080bcc197d80
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-24 22:00:27 -0500

    CRUD - Agregado con estilos

--- src/app/components/list-product/list-product.component.ts
+++ src/app/components/list-product/list-product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Producto } from 'src/app/interfaces/producto';
 import { ProductoService } from 'src/app/service/producto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-product',
@@ -20,7 +21,25 @@ export class ListProductComponent {
 
   }
 
-onDeleteProduct(producto:Producto){
-  this.productoService.deleteProduct(producto)
-}
+  onDeleteProduct(producto: Producto) {
+
+    Swal.fire({
+      title: 'Desea eliminar producto',
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Si, Eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Exitoso!',
+          'Has Eliminado producto exitosamente',
+          'success'
+        )
+        this.productoService.deleteProduct(producto)
+      }
+    })
+
+  }
 }
--- src/app/components/register-product/register-product.component.ts
+++ src/app/components/register-product/register-product.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/service/producto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register-product',
@@ -10,7 +11,7 @@ import { ProductoService } from 'src/app/service/producto.service';
 })
 export class RegisterProductComponent implements OnInit {
   formRegisterProduct!: FormGroup
-  constructor(private productService: ProductoService, private fb: FormBuilder, private route:Router) { }
+  constructor(private productService: ProductoService, private fb: FormBuilder, private route: Router) { }
 
   ngOnInit(): void {
     this.formRegisterProduct = this.fb.group({
@@ -21,10 +22,30 @@ export class RegisterProductComponent implements OnInit {
     })
   }
 
-  saveProduct() {
-    this.productService.addProduct(this.formRegisterProduct.value)
-    alert("Registrado exitosamente")
-    this.route.navigate(['/'])
+  async saveProduct() {
+    if (this.formRegisterProduct.valid) {
+      this.productService.addProduct(this.formRegisterProduct.value)
+        .then(res => {
+          if (res) {
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: `Se ha regitrado correctamente al producto`,
+              showConfirmButton: false,
+              timer: 1000
+            })
+            this.route.navigate(['/home'])
+          }
+        })
+        .catch(error => { console.log(error) })
+
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: '',
+        text: 'Los campos no deben estar vacios!'
+      })
+    }
   }
 
 }
